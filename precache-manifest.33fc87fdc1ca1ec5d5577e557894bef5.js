5f586c96fd81923babcc5d8ae008646b19ba8627
HarisKumar <hariskumarsivashanmugam@gmail.com>
2020-05-25 21:13:07 +0530

    Updates

---
+++ precache-manifest.33fc87fdc1ca1ec5d5577e557894bef5.js
@@ -0,0 +1,114 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e872ded023a698d732e2fd61fb1fc5dc",
+    "url": "/Mav/index.html"
+  },
+  {
+    "revision": "f55012f0f04855c528e9",
+    "url": "/Mav/static/css/2.81a3c6ce.chunk.css"
+  },
+  {
+    "revision": "ec13f808754d81db33e0",
+    "url": "/Mav/static/css/main.aac56212.chunk.css"
+  },
+  {
+    "revision": "f55012f0f04855c528e9",
+    "url": "/Mav/static/js/2.c2f61542.chunk.js"
+  },
+  {
+    "revision": "4d47ffde3dc69784e8e23fd910817ef8",
+    "url": "/Mav/static/js/2.c2f61542.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ec13f808754d81db33e0",
+    "url": "/Mav/static/js/main.b71b4b8f.chunk.js"
+  },
+  {
+    "revision": "f7a3011471f19b343ab4",
+    "url": "/Mav/static/js/runtime-main.e259b3a4.js"
+  },
+  {
+    "revision": "13db00b7a34fee4d819ab7f9838cc428",
+    "url": "/Mav/static/media/brand-icons.13db00b7.eot"
+  },
+  {
+    "revision": "a046592bac8f2fd96e994733faf3858c",
+    "url": "/Mav/static/media/brand-icons.a046592b.woff"
+  },
+  {
+    "revision": "a1a749e89f578a49306ec2b055c073da",
+    "url": "/Mav/static/media/brand-icons.a1a749e8.svg"
+  },
+  {
+    "revision": "c5ebe0b32dc1b5cc449a76c4204d13bb",
+    "url": "/Mav/static/media/brand-icons.c5ebe0b3.ttf"
+  },
+  {
+    "revision": "e8c322de9658cbeb8a774b6624167c2c",
+    "url": "/Mav/static/media/brand-icons.e8c322de.woff2"
+  },
+  {
+    "revision": "9c74e172f87984c48ddf5c8108cabe67",
+    "url": "/Mav/static/media/flags.9c74e172.png"
+  },
+  {
+    "revision": "0ab54153eeeca0ce03978cc463b257f7",
+    "url": "/Mav/static/media/icons.0ab54153.woff2"
+  },
+  {
+    "revision": "8e3c7f5520f5ae906c6cf6d7f3ddcd19",
+    "url": "/Mav/static/media/icons.8e3c7f55.eot"
+  },
+  {
+    "revision": "962a1bf31c081691065fe333d9fa8105",
+    "url": "/Mav/static/media/icons.962a1bf3.svg"
+  },
+  {
+    "revision": "b87b9ba532ace76ae9f6edfe9f72ded2",
+    "url": "/Mav/static/media/icons.b87b9ba5.ttf"
+  },
+  {
+    "revision": "faff92145777a3cbaf8e7367b4807987",
+    "url": "/Mav/static/media/icons.faff9214.woff"
+  },
+  {
+    "revision": "c1109efaabcad2012d07b03dfd8430bd",
+    "url": "/Mav/static/media/logo_maverickio.c1109efa.png"
+  },
+  {
+    "revision": "701ae6abd4719e9c2ada3535a497b341",
+    "url": "/Mav/static/media/outline-icons.701ae6ab.eot"
+  },
+  {
+    "revision": "82f60bd0b94a1ed68b1e6e309ce2e8c3",
+    "url": "/Mav/static/media/outline-icons.82f60bd0.svg"
+  },
+  {
+    "revision": "ad97afd3337e8cda302d10ff5a4026b8",
+    "url": "/Mav/static/media/outline-icons.ad97afd3.ttf"
+  },
+  {
+    "revision": "cd6c777f1945164224dee082abaea03a",
+    "url": "/Mav/static/media/outline-icons.cd6c777f.woff2"
+  },
+  {
+    "revision": "ef60a4f6c25ef7f39f2d25a748dbecfe",
+    "url": "/Mav/static/media/outline-icons.ef60a4f6.woff"
+  },
+  {
+    "revision": "7e76f8a5af8560155266c6873c7debb5",
+    "url": "/Mav/static/media/profileImage.7e76f8a5.jpg"
+  },
+  {
+    "revision": "db8dc4e15a7c87674e92c4fffdc5d7cb",
+    "url": "/Mav/static/media/upload.db8dc4e1.png"
+  },
+  {
+    "revision": "616fcc9ddf4239162b015afc00c52364",
+    "url": "/Mav/static/media/user.616fcc9d.png"
+  },
+  {
+    "revision": "764962eae1ba57a0b533405a21b4ef95",
+    "url": "/Mav/static/media/video.764962ea.jpg"
+  }
+]);
